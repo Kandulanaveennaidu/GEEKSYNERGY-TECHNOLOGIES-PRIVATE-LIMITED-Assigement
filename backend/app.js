9ed6e1f4228a6d77562cdf7d1923b36a8b0773da
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-07-04 11:06:51 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -19,5 +19,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
